fb2573d32823a1d90d2538049a4e11539bf5b6f1
Ridho <muhammadridhoputra@gmail.com>
2020-06-22 14:36:59 +0700

    add more config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,3 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
   plugins: [
     `gatsby-plugin-netlify-cms`,
@@ -18,7 +12,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `content`,
-        path: `${__dirname}/src/content`,
+        path: `${__dirname}/content`,
       },
     },
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,33 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-export const query = graphql`
-  query {
-    allFile(
-      filter: { sourceInstanceName: { eq: "content" }, name: { eq: "home" } }
-    ) {
-      edges {
-        node {
-          childMarkdownRemark {
-            frontmatter {
-              title
-              intro
-              image {
-                id
-                relativePath
-                absolutePath
-                root
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query {
+//     allFile(
+//       filter: { sourceInstanceName: { eq: "content" }, name: { eq: "home" } }
+//     ) {
+//       edges {
+//         node {
+//           childMarkdownRemark {
+//             frontmatter {
+//               title
+//               intro
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
 
 export default function Home(props) {
-  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
-  console.log(data)
+  // const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  // console.log(data)
 
   return (
     <>
-      <h1>{data.title}</h1>
-      <p>{data.intro}</p>
+      {/* <h1>{data.title}</h1>
+      <p>{data.intro}</p> */}
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}></div>
     </>
   )
